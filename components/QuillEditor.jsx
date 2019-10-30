211e4f39e679564b5597e0f8b9698e7f7eb1313b
Yisrael Grimes <yisraelgrimes@gmail.com>
2019-10-30 12:58:48 -0500

    basic react-quill test

--- components/QuillEditor.jsx
+++ components/QuillEditor.jsx
@@ -1,36 +1,59 @@
 import React from "react";
+import Head from "next/head";
+import ReactQuill from "react-quill";
 
-import "quill/dist/quill.snow.css";
+// import "quill/dist/quill.snow.css";
 
-import Quill from "quill/core";
-import Toolbar from "quill/modules/toolbar";
-import Snow from "quill/themes/snow";
+// import Quill from "quill/core";
+// import Toolbar from "quill/modules/toolbar";
+// import SnowTheme from "quill/themes/snow";
 
 export default class QuillEditor extends React.Component {
-
-	componentDidMount() {
-
-		Quill.register({
-			"themes/snow": Snow,
-			"modules/toolbar": Toolbar,
-		});
-
-		new Quill("#editor", {
-			theme: "snow",
-			placeholder: "Write something awesome...",
-		});
+	constructor(props) {
+		super(props);
+		this.state = { text: "" }; // You can also pass a Quill Delta here
+		this.handleChange = this.handleChange.bind(this);
 	}
+
+	handleChange(value) {
+		this.setState({ text: value });
+	}
+
+	// componentDidMount() {
+
+	// 	Quill.register({
+	// 		"themes/snow": SnowTheme,
+	// 		"modules/toolbar": Toolbar,
+	// 	});
+
+	// 	new Quill("#editor", {
+	// 		theme: "snow",
+	// 		placeholder: "Write something awesome...",
+	// 	});
+	// }
 
 	render() {
 		return (
-			<div>
-				<div id="QuillEditor-container">
-					<div id="editor">
-						<p>Hello World!</p>
-						<p>Some initial <strong>bold</strong> text</p>
-					</div>
+			<>
+				<Head>
+					<link rel="stylesheet" href="//cdn.quilljs.com/1.2.6/quill.snow.css" />
+					<title>Quill Test</title>
+				</Head>
+				<div>
+					<ReactQuill value={this.state.text} onChange={this.handleChange} />
+
+					<pre style={{background:"#333", color:"white", margin:"30px 5px", padding:"10px"}}>
+						{JSON.stringify(this.state, null, 2)}
+					</pre>
+
+					{/* <div id="QuillEditor-container">
+						<div id="editor">
+							<p>Hello World!</p>
+							<p>Some initial <strong>bold</strong> text</p>
+						</div>
+					</div> */}
 				</div>
-			</div>
+			</>
 		);
 	}
 }
