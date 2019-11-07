b2c69d26e57029fe5cec3ef0c7d94a6f44adbaa0
IvaTsu <ivan.tsud@gmail.com>
2019-11-07 21:35:46 +0200

    Update component name

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,13 @@
 import dynamic from "next/dynamic";
 
-const DynamicComponentWithNoSSR = dynamic(
+const EditorWithNoSSR = dynamic(
 	() => import("../components/QuillEditor"),
 	{ ssr: false }
 );
 
 function Home() {
 	return (
-		<div>
-			<DynamicComponentWithNoSSR />
-		</div>
+		<EditorWithNoSSR />
 	);
 }
 
